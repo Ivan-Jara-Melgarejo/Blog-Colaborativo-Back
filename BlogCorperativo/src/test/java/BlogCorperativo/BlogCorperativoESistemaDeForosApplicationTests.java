caa16ca8a481140892ab75857eac7e4dd39421e8
Ivan Jara Melgarejo <ivanjaramelgarejo2020@gmail.com>
2024-09-10 22:59:22 -0300

    first commit

---
+++ BlogCorperativo/src/test/java/BlogCorperativo/BlogCorperativoESistemaDeForosApplicationTests.java
@@ -0,0 +1,13 @@
+package BlogCorperativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCorperativoESistemaDeForosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
