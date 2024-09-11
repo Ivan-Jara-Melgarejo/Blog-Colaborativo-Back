641e3aabc47e9e1526f5bedb77ed889a6de98a67
Ivan Jara Melgarejo <ivanjaramelgarejo2020@gmail.com>
2024-09-10 23:59:42 -0300

    First Commit :)

---
+++ BlogColaborativo/src/main/java/BlogColaborativo/BlogColaborativoESistemaDeForosApplication.java
@@ -0,0 +1,13 @@
+package BlogColaborativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogColaborativoESistemaDeForosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogColaborativoESistemaDeForosApplication.class, args);
+	}
+
+}
---
+++ BlogColaborativo/src/test/java/BlogColaborativo/BlogColaborativoESistemaDeForosApplicationTests.java
@@ -0,0 +1,13 @@
+package BlogColaborativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogColaborativoESistemaDeForosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
